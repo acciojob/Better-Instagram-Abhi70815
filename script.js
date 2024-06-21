81239407f7f60099d6639e22b85152215d05fe5f
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-21 22:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+ const divs = document.querySelectorAll('.image-placeholder');
+
+        divs.forEach((div) => {
+            div.addEventListener('dragstart', (e) => {
+                e.dataTransfer.setData('text/plain', div.id);
+            });
+
+            div.addEventListener('dragover', (e) => {
+                e.preventDefault();
+            });
+
+            div.addEventListener('drop', (e) => {
+                e.preventDefault();
+                const sourceDivId = e.dataTransfer.getData('text/plain');
+                const targetDivId = div.id;
+
+                // Swap background images between divs
+                const sourceDiv = document.getElementById(sourceDivId);
+                const targetDiv = document.getElementById(targetDivId);
+                const tempImage = sourceDiv.style.backgroundImage;
+                sourceDiv.style.backgroundImage = targetDiv.style.backgroundImage;
+                targetDiv.style.backgroundImage = tempImage;
+            });
+        });
