771b9dd50e7a3dd6b51d054d4835c3840862503b
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-09 11:37:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-window.onload=function(){
-const divs = document.querySelectorAll('.image');
+window.onload = function() {
+  let divs = document.querySelectorAll('.image');
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].setAttribute('id', 'div' + (i + 1));
+    divs[i].addEventListener("dragstart", handleDragStart, false);
+    divs[i].addEventListener("dragover", handleDragOver, false);
+    divs[i].addEventListener("drop", handleDrop, false);
+  }
+}
 
-divs.forEach((div) => {
-  div.setAttribute('draggable', true); // Make elements draggable
+let dragSrcEl = null;
 
-  div.addEventListener('dragstart', (e) => {
-    e.dataTransfer.setData('text', div.id);
-  });
+function handleDragStart(e) {
+  
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+}
 
-  div.addEventListener('dragover', (e) => {
+function handleDragOver(e) {
+  if (e.preventDefault) {
     e.preventDefault();
-    e.stopPropagation(); // Prevent default behavior and event propagation
-  });
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
 
-  div.addEventListener('drop', (e) => {
-    e.preventDefault();
+function handleDrop(e) {
+  if (e.stopPropagation) {
     e.stopPropagation();
-
-    const sourceDivId = e.dataTransfer.getData('text');
-    const targetDivId = e.target.id; // Get the ID of the target element
-
-    // Make sure that e.target is one of the div elements
-    if (Array.from(divs).some(div => div === e.target)) {
-      // Swap background images between source and target divs
-      const sourceDiv = document.getElementById(sourceDivId);
-      const targetDiv = e.target;
-
-      // Get computed styles
-      const sourceImage = window.getComputedStyle(sourceDiv).backgroundImage;
-      const targetImage = window.getComputedStyle(targetDiv).backgroundImage;
-
-      // Parse the backgroundImage strings to extract the image URLs
-      const sourceImageUrl = sourceImage.match(/url\("(.+)"\)/)[1];
-      const targetImageUrl = targetImage.match(/url\("(.+)"\)/)[1];
-
-      // Swap the images
-      sourceDiv.style.backgroundImage = `url("${targetImageUrl}")`;
-      targetDiv.style.backgroundImage = `url("${sourceImageUrl}")`;
+  }
+  if (dragSrcEl != this) {
+    let parent = document.querySelector('#parent');
+    let srcIndex = Array.prototype.indexOf.call(parent.children, dragSrcEl);
+    let targetIndex = Array.prototype.indexOf.call(parent.children, this);
+    if (srcIndex < targetIndex) {
+      parent.insertBefore(dragSrcEl, this.nextSibling);
+      parent.insertBefore(this, parent.children[srcIndex]);
+    } else {
+      parent.insertBefore(this, dragSrcEl);
+      parent.insertBefore(dragSrcEl, parent.children[targetIndex]);
     }
-  });
-});
+  }
+  return false;
 }
